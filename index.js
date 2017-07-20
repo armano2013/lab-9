5982b1da3c42d4d8d793fb79ac88ffa1b2bb7068
Mohammed Ali <fh0420@wayne.edu>
2017-07-20 19:26:01 -0400

    Seperate the JS

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
+var message;
 $.get('https://www.reddit.com/r/aww/.json', function(responseBody){
-  $function(elment){
-
-
-
-
-
-
-    $(document).ready(function(){
-        $("button").click(function(){
-            $("#div1").animate({
-                height: 'toggle'
-            });
-            $("#div2").animate({
-                height: 'toggle'
-            });
-            $("#div3").animate({
-                height: 'toggle'
-            });
-            $("#div4").animate({
-                height: 'toggle'
-            });
-            $("#div5").animate({
-                height: 'toggle'
-            });
-        });
+    message = responseBody;
+    console.log(responseBody);
+    $( "body" ).data.children.({ Array: [25]}).Object.author;
+});
+console.log(message); // > undefined
+ // > undefined
+$("button").click(function(){
+    $("#1").animate({
+        height: 'toggle'
     });
-
-
-
-
+    $("#2").animate({
+        height: 'toggle'
+    });
+    $("#3").animate({
+        height: 'toggle'
+    });
+    $("#4").animate({
+        height: 'toggle'
+    });
+    $("#5").animate({
+        height: 'toggle'
+    });
+    $("#6").animate({
+        height: 'toggle'
+    });
+    $("#7").animate({
+        height: 'toggle'
+    });
+    $("#8").animate({
+        height: 'toggle'
+    });
+    $("#9").animate({
+        height: 'toggle'
+    });
+    $("#10").animate({
+        height: 'toggle'
+    });
 });
-});
+
+//Andrew
+
+
+
+
+
+//Mohammed
+
+
+
+
+//Brandon
