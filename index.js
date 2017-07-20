b0efbffa1ea165d2bd7460aa5699d7b6ab5e893b
Mohammed Ali <fh0420@wayne.edu>
2017-07-20 13:29:43 -0400

    toggle div added

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 $.get('https://www.reddit.com/r/aww/.json', function(responseBody){
-    console.log(responseBody.data.children);
-  });
+  $function(elment){
+
+
+
+
+
+
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div1").animate({
+                height: 'toggle'
+            });
+            $("#div2").animate({
+                height: 'toggle'
+            });
+            $("#div3").animate({
+                height: 'toggle'
+            });
+            $("#div4").animate({
+                height: 'toggle'
+            });
+            $("#div5").animate({
+                height: 'toggle'
+            });
+        });
+    });
+
+
+
+
+});
+});
