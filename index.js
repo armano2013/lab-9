b37b42501190c4ae21b7a7f3a7b7a4591b58a99f
Mohammed Ali <fh0420@wayne.edu>
2017-07-22 10:44:04 -0400

    fix the formate of JS

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
     $.get('https://www.reddit.com/r/aww/.json', function(responseBody){
 
         responseBody.data.children.forEach(function(item, index){
-
-            $('#fs'+index).html('<img src = "'+item.data.thumbnail+'" height = "200px" width = "290px" />'+'Author: '+item.data.author+'<legend>'+"Title: "+item.data.title+'</legend>');
+            var tag ='<img src = "'+item.data.thumbnail+'" height = "200px" width = "290px" />'+
+            'Author: '+item.data.author+
+            '<legend>'+"Title: "+item.data.title+'</legend>';
+            $('#fs'+index).html(tag);
 
         });
 
